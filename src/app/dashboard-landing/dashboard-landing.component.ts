79f4be6b5dbed186250253b8bd91335028c1ccf3
Shyam-prasad2000 <shyamprasad2000.sp@gmail.com>
2024-12-01 00:39:31 +0530

    initial project setup and ui set up

---
+++ src/app/dashboard-landing/dashboard-landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-landing',
+  imports: [],
+  templateUrl: './dashboard-landing.component.html',
+  styleUrl: './dashboard-landing.component.css'
+})
+export class DashboardLandingComponent {
+
+}
